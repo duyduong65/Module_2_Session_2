af8b2676e257703e48c15a85a2cc90c282d7f4af
duyduong65 <tg.bluesky66@gmail.com>
2019-10-03 15:15:28 +0700

    add find min and max Number

---
+++ T_findMinNumberWithMethod/index.php
@@ -0,0 +1,35 @@
+<?php
+$arrNumber = [];
+for ($i = 0; $i < 50; $i++) {
+    $randomNumber = random_int(1, 50);
+    array_push($arrNumber, $randomNumber);
+//    echo $arrNumber[$i] . "<br>";
+}
+
+class Number
+{
+    private $arr;
+
+    public function __construct($arr)
+    {
+        $this->arr = $arr;
+    }
+
+    public function getMinNumber()
+    {
+        $min = $this->arr[0];
+        foreach ($this->arr as $number) {
+            if ($min > $number) {
+                $min = $number;
+            }
+        }
+        return $min;
+    }
+}
+
+;
+
+$objNumber = new Number($arrNumber);
+echo $objNumber->getMinNumber();
+
+?>
