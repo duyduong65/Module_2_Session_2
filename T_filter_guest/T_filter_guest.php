015947c50bd8b7861cb241b83db41bd206177144
duyduong65 <tg.bluesky66@gmail.com>
2019-10-03 11:10:03 +0700

    add filter guest

--- T_filter_guest/T_filter_guest.php
+++ T_filter_guest/T_filter_guest.php
@@ -1,1 +1,47 @@
 <?php
+$customer_list = [
+    "0" => array("name" => "Mai Văn Hoàn", "day_of_birth" => "1983/08/20", "address" => "Hà Nội", "images" => "images/img1.png"),
+    "1" => array("name" => "Nguyễn Văn Nam", "day_of_birth" => "1983/08/21", "address" => "Bắc Giang", "images" => "images/img2.png"),
+    "2" => array("name" => "Nguyễn Thái Hòa", "day_of_birth" => "1983/08/22", "address" => "Nam Định", "images" => "images/img3.png"),
+    "3" => array("name" => "Trần Đăng Khoa", "day_of_birth" => "1983/08/17", "address" => "Hà Tây", "images" => "images/img4.png"),
+    "4" => array("name" => "Nguyễn Đình Thi", "day_of_birth" => "1983/08/19", "address" => "Hà Nội", "images" => "images/img5.png")
+];
+
+function searchByDate($customers, $fromDate, $toDate)
+{
+    if (empty($fromDate) && empty($toDate)) {
+        return $customers;
+    }
+    $filtered_customer = [];
+    foreach ($customers as $customer) {
+        if (!empty($fromDate) && (strtotime($customer['day_of_birth']) < strtotime($fromDate))) {
+            continue;
+        }
+        if (!empty($toDate) && (strtotime($customer['day_of_birth']) > strtotime($toDate))) {
+            continue;
+        }
+        $filtered_customer[] = $customer;
+    }
+    return $filtered_customer;
+}
+
+$fromDate = NULL;
+$toDate = NULL;
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    $fromDate = $_POST['from'];
+    $toDate = $_POST['to'];
+}
+$filtered_customer = searchByDate($customer_list, $fromDate, $toDate);
+?>
+
+<?php foreach ($filtered_customer as $index => $customer): ?>
+    <tr>
+    <td><?php echo $index + 1 ?></td>
+    <td><?php echo $customer['name'] ?></td>
+    <td><?php echo $customer['day_of_birth'] ?></td>
+    <td><?php echo $customer['address'] ?></td>
+    <td>
+        <div class="profile"><img src=<?php echo $customer['images'] ?> width="50" height="50"></div>
+    </td>
+    </tr>
+<?php endforeach; ?>
--- T_filter_guest/index.php
+++ T_filter_guest/index.php
@@ -6,8 +6,24 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-
+<form action="" method="post">
+    FROM: <input type="text" name="from" placeholder="yyyyy/mm/dd">
+    TO: <input type="text" name="to" placeholder="yyyy/mm/dd">
+    <input type="submit" value="search">
+    <table>
+        <caption><h1>Customer List</h1></caption>
+        <tr>
+            <td>No.</td>
+            <td>Customer name</td>
+            <td>Birthday</td>
+            <td>Address</td>
+            <td>Image</td>
+        </tr>
+    <?php include "T_filter_guest.php"?>
+    </table>
+</form>
 </body>
 </html>
